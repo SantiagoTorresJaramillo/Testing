6b95d5cb31049698628698cf73f011c266fe7aed
Santiago <santiago.torres@dailybot.com>
2023-08-16 22:17:39 +0000

    Add multiple browser's image

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "cypress"
 
 export default defineConfig({
+  projectId: '',
+    videoCompression: false,
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+    responseTimeout: 30000,
+    viewportWidth: 1280,
+    viewportHeight: 720,
   e2e: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
+    retries: {
+      runMode: 3,
+      openMode: 0,
+    }
   },
 })
