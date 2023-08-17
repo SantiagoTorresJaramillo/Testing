d18e31532ec044b45dcee6ed5d273c98cc90f640
Santiago <santiago.torres@dailybot.com>
2023-08-17 21:17:13 +0000

    Testing

--- cypress/e2e/visit.cy.ts
+++ cypress/e2e/visit.cy.ts
@@ -1,7 +1,13 @@
 describe('User Onboarding', () => {
   it('can navigate to the site', () => {
+    const email = Cypress.env('EMAIL')
+    const password = Cypress.env('PASSWORD')
+    
     cy.visit('https://app.dailybot.com/')
       .contains('Log in')
       .should('be.visible')
+      
+    cy.get('#input-email').type(email)
+    cy.get('#input-password').type(password)
   })
 })
